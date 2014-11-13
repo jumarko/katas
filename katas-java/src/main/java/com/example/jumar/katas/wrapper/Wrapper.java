e5ea50fe6c46101f869b1b58199b8ea9ceab92cd
Juraj Martinka <juraj.martinka@gooddata.com>
2014-11-13 09:19:25 +0100

    Wrapper kata - intermediate result using recursion and last index of space.

--- katas-java/src/main/java/com/example/jumar/katas/wrapper/Wrapper.java
+++ katas-java/src/main/java/com/example/jumar/katas/wrapper/Wrapper.java
@@ -1,7 +1,29 @@
 package com.example.jumar.katas.wrapper;
 
 public class Wrapper {
+
     public static String wrap(String string, int columnNumber) {
-        return string;
+
+        if (string.length() <= columnNumber) {
+
+            return string;
+
+        }
+
+        final int lastSpaceIndex = string.substring(0, columnNumber).lastIndexOf(' ');
+
+        if (lastSpaceIndex != -1) {
+
+            return string.substring(0, lastSpaceIndex)
+                    + "\n"
+                    + wrap(string.substring(lastSpaceIndex + 1), columnNumber);
+
+        } else {
+
+            return string.substring(0, columnNumber)
+                    + "\n"
+                    + wrap(string.substring(columnNumber), columnNumber);
+        }
+
     }
 }
--- katas-java/src/test/java/com/example/jumar/katas/wrapper/WrapperTest.java
+++ katas-java/src/test/java/com/example/jumar/katas/wrapper/WrapperTest.java
@@ -4,24 +4,84 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        WrapperTest.DegenerateTests.class,
+        WrapperTest.WrapWordsTest.class
+})
 public class WrapperTest {
 
-    @Test
-    public void wrapEmptyString() {
-        checkWrap("", 0, "");
-        checkWrap("", 1, "");
-        checkWrap("", 10, "");
-    }
+     public static class DegenerateTests {
+
+         @Test
+         public void wrapEmptyString() {
+             checkWrap("", 0, "");
+             checkWrap("", 1, "");
+             checkWrap("", 10, "");
+         }
 
 
+         @Test
+         public void wrapStringShorterThanColumnNumber() {
+             checkWrap("ahoj", 5, "ahoj");
+             checkWrap("Len tak", 7, "Len tak");
+             checkWrap("abeceda", 8, "abeceda");
+         }
 
-    @Test
-    public void wrapStringShorterThanColumnNumber() {
-        checkWrap("ahoj", 5, "ahoj");
-        checkWrap("Len tak", 7, "Len tak");
-        checkWrap("abeceda", 8, "abeceda");
-    }
+     }
+
+     public static class WrapWordsTest {
+
+         @Test
+         public void splitOneWord() {
+             checkWrap("word", 2, "wo\nrd");
+         }
+
+         @Test
+         public void splitOneWordTwoTimes() {
+             checkWrap("oneword", 3, "one\nwor\nd");
+         }
+
+         @Test
+         public void splitOneWordManyTimes() {
+             checkWrap("onesingleword", 3, "one\nsin\ngle\nwor\nd");
+         }
+
+         @Test
+         public void wrapOnWordBoundary() {
+             checkWrap("word word", 5, "word\nword");
+         }
+
+         @Test
+         public void wrapThreeWordsBoundary() {
+             checkWrap("word word word", 5, "word\nword\nword");
+         }
+
+         @Test
+         public void wrapTwoWordsAfterSpace() {
+             checkWrap("word word", 6, "word\nword");
+         }
+//
+//         @Test
+//         public void wrapThreeWordsAfterFirstSpace() {
+//             checkWrap("word word word", 6, "word\nword\nword");
+//         }
+//
+//         @Test
+//         public void wrapThreeWordsAfterSecondSpace() {
+//             checkWrap("word word word", 11,
+//                     "word word\nword");
+//         }
+//
+//         @Test
+//         public void wrapWordsAfterSpace() {
+//             checkWrap("Ahoj len tak som si vsimol, ze robis nieco zaujimave", 10,
+//                     "Ahoj len\ntak som si\nvsimol, ze\nrobis\nnieco\nzaujimave");
+//         }
+     }
 
 
     private static void checkWrap(String inputString, int columnNumber, String expectedString) {
